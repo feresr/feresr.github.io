6895cbcdc56c336824fb28625f4ce1943a902031
Fernando Raviola <fernandoraviola@gmail.com>
2020-10-28 19:23:57 -0300

    make interactive

--- js/index.js
+++ js/index.js
@@ -11,9 +11,10 @@ precision mediump float;
 
 uniform vec2 iResolution;
 uniform float iTime;
+uniform vec2 iMouse;
 
 const float THRESHOLD = 0.9;
-const int CIRCLE_COUNT = 5;
+const int CIRCLE_COUNT = 6;
 
 void main()
 {
@@ -23,11 +24,14 @@ void main()
     };
     Circle circles[CIRCLE_COUNT];
 
-    circles[0] = Circle(vec2(0.05, 0.26), .55);     // fixed
-    circles[1] = Circle(vec2(0.25, -0.68), .37);    // down fixed
-    circles[2] = Circle(vec2(0.0, 0.0), .58);
-    circles[3] = Circle(vec2( -0.1, 0.5), .40);
-    circles[4] = Circle(vec2(0.0    , -0.45), .5); // down
+    
+    circles[0] = Circle(vec2(-0.25, 0.26), .55);     // fixed
+    circles[1] = Circle(vec2(-.1, -0.68), .37);    // down fixed
+    circles[2] = Circle(vec2(-.25, 0.2), .58);
+    circles[3] = Circle(vec2( -0.33, 0.5), .40);
+    circles[4] = Circle(vec2(-.26    , -0.45), .5); // down
+    circles[5] = Circle(iMouse * 2.0, .3); //mouse controlled
+    
         
     // Normalized pixel coordinates (from 0 to 1)
     vec2 uv = (2.0 * (gl_FragCoord.xy) - iResolution.xy)/iResolution.y;
@@ -47,7 +51,8 @@ void main()
         circle.pos.y += dy * sin(float(i + 1) * iTime); 
         
         float dist = length(uv - circle.pos);
-    	f += smoothstep(circle.radius, circle.radius - THRESHOLD, dist);
+        
+        f += smoothstep(circle.radius, circle.radius - THRESHOLD, dist);
 	}
    
     if (f >= .05) {
@@ -60,17 +65,38 @@ void main()
 `
 
 var gl;
+var mousex = 500;
+var mousey = -80;
 
 var loadBackground = function () {
 
     var canvas = document.getElementById('surface');
+
+    const getMousePos = (evt) => {
+        const pos = evt.currentTarget.getBoundingClientRect();
+        return {
+          x: evt.clientX - pos.left,
+          y: evt.clientY - pos.top
+        };
+      };
+      
+
+      document.querySelector("#header").style.cursor = "none";
+    document.querySelector("#header").addEventListener('mousemove', (evt) => {
+        
+        const mPos = getMousePos(evt);
+        
+        mousex = mPos.x;
+        mousey = -mPos.y;
+        
+      });
 
     gl = canvas.getContext('webgl');
 
     window.addEventListener('resize', resizeCanvas, false);
 
     function resizeCanvas() {
-            canvas.width = Math.max(window.innerWidth / 3, 500);
+            canvas.width = Math.max(window.innerWidth / 2, 500);
             canvas.height = window.innerHeight;
             
             if (!gl) {
@@ -145,10 +171,10 @@ var loadBackground = function () {
     gl.useProgram(program);
 
     var timeLocation = gl.getUniformLocation(program, "iTime");
+    var mouseLocation = gl.getUniformLocation(program, "iMouse");
     var resolutionLocation = gl.getUniformLocation(program, "iResolution");
     function draw(now) {
-        
-        console.log(window.innerWidth/window.innerHeight);
+
         if (window.innerWidth/window.innerHeight < 1.25) {
             canvas.width = 0;
             canvas.height = 0;
@@ -161,8 +187,14 @@ var loadBackground = function () {
         gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
         now *= 0.0001;
+
         gl.uniform1f(timeLocation, now);
         gl.uniform2f(resolutionLocation, gl.canvas.width, gl.canvas.height);
+
+
+        console.log(mousex / gl.canvas.width - .5);
+
+        gl.uniform2f(mouseLocation, mousex / gl.canvas.width - .5, mousey / gl.canvas.height + .5);
         gl.drawArrays(gl.TRIANGLES, 0, 6);
     
         requestAnimationFrame(draw);
